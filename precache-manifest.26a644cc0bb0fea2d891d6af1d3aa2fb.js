ec4832e11da5ea65dfc34d9c98ccc9eb11501444
programmer-er <jhariharan2004@gmail.com>
2023-01-30 17:00:15 +0530

    Updates

---
+++ precache-manifest.26a644cc0bb0fea2d891d6af1d3aa2fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b0834a2b80c99d29147",
+    "url": "/csd-robocon-nitk.github.io/static/css/main.09036b8c.chunk.css"
+  },
+  {
+    "revision": "9b0834a2b80c99d29147",
+    "url": "/csd-robocon-nitk.github.io/static/js/main.34d69ca0.chunk.js"
+  },
+  {
+    "revision": "980f9039f59269776590",
+    "url": "/csd-robocon-nitk.github.io/static/js/runtime~main.48416086.js"
+  },
+  {
+    "revision": "4a867a62c1e308445274",
+    "url": "/csd-robocon-nitk.github.io/static/js/2.f60f5818.chunk.js"
+  },
+  {
+    "revision": "b9d79730f362b9538d205a6715235eca",
+    "url": "/csd-robocon-nitk.github.io/static/media/svg-3.b9d79730.svg"
+  },
+  {
+    "revision": "0f67260bd0df5d0fed930b27b9259060",
+    "url": "/csd-robocon-nitk.github.io/static/media/robocon.0f67260b.mp4"
+  },
+  {
+    "revision": "7e253e54499684f52cd994929e55764a",
+    "url": "/csd-robocon-nitk.github.io/static/media/logo.7e253e54.png"
+  },
+  {
+    "revision": "cf930eef7a125a7d4945bce5f1c7eb69",
+    "url": "/csd-robocon-nitk.github.io/static/media/svg-5.cf930eef.svg"
+  },
+  {
+    "revision": "4ee7c04d824d16d08021fab3f5da8bfc",
+    "url": "/csd-robocon-nitk.github.io/static/media/svg-2.4ee7c04d.svg"
+  },
+  {
+    "revision": "31efadbb1a1a99b33085c3454611afbf",
+    "url": "/csd-robocon-nitk.github.io/static/media/collage.31efadbb.png"
+  },
+  {
+    "revision": "1ff811877384d774110b282e4c4a94a5",
+    "url": "/csd-robocon-nitk.github.io/static/media/abu.1ff81187.webp"
+  },
+  {
+    "revision": "29ac1780622042b18533f6655d9288da",
+    "url": "/csd-robocon-nitk.github.io/index.html"
+  }
+];
