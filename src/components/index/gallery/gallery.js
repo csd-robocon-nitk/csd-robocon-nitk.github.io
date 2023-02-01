e3ed8bf1875897ee2e66f5ab65b54d07d8a2894a
programmer-er <jhariharan2004@gmail.com>
2023-02-01 19:57:41 +0530

    Added staggered animated grid gallery on home page

--- src/components/index/gallery/gallery.js
+++ src/components/index/gallery/gallery.js
@@ -1,13 +1,38 @@
 import './gallery.css'
-import React from 'react'
-import collage from '../../../images/collage.png'
+import React, { useEffect, useRef, useState } from 'react'
+import img1 from '../../../images/001.jpg'
+import img2 from '../../../images/002.jpg'
+import img3 from '../../../images/003.jpg'
+import img4 from '../../../images/001.jpg'
+import img5 from '../../../images/002.jpg'
+import img6 from '../../../images/003.jpg'
 import { Link } from 'react-router-dom'
 
 export default function GallerySection () {
+
+    let [visible, setVisible] = useState(false)
+    let images = useRef(0)
+
+    let observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting)
+            setVisible(true)
+        // else
+        //     setVisible(false)
+    })
+
+    useEffect(_ =>
+        observer.observe(images.current)
+    )
+
     return (
-        <div id="gallery" className="light">
-            <div id="left">
-                <img src={collage} />
+        <div id="gallery" className="dark">
+            <div id="left" className={visible ? "visible" : undefined} ref={images}>
+                <img src={img1} />
+                <img src={img2} />
+                <img src={img3} />
+                <img src={img4} />
+                <img src={img5} />
+                <img src={img6} />
             </div>
             <div id="right">
                 <h1>GALLERY</h1>
