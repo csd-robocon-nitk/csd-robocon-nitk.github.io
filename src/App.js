0b58115f290842d7cac7c51d1b4ab98cc25aaa54
programmer-er <jhariharan2004@gmail.com>
2023-02-03 19:17:59 +0530

    Cleanup - purged unused files - compressed background video - removed unused imports - made hero element height fill the screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages";
-import SininPage from "./components/pages/signin";
 import React from 'react'
 import Gallery from "./components/pages/gallery";
 import Team from './components/pages/team'
@@ -12,7 +11,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/gallery" element={<Gallery />} />
-                <Route path="/signin" element={<SininPage />} />
                 <Route path="/team" element={<Team />} />
             </Routes>
         </Router>
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import HeroSection from "../HeroSection";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
-import Services from "../Services";
 import Footer from "../Footer";
 import GallerySection from '../index/gallery/gallery'
 import AboutSection from "../index/about/about";
