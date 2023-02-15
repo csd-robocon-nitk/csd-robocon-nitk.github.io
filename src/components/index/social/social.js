7db365ae71002d03adc425cd75e4aaf92da77317
Shriramu2002 <shriramu2002@gmail.com>
2023-02-16 01:47:58 +0530

    instagram posts apiPart done

--- src/components/index/social/social.js
+++ src/components/index/social/social.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import './social.css'
+import axios from 'axios';
+import { useEffect,useRef } from 'react';
 
 export default function SocialSection () {
+
+let posts= useRef();
+useEffect(()=>{
+const apiUrl = "https://graph.instagram.com/me/media";
+const accessToken = "IGQVJYN0swUVQtNzNBNjhIdDI2UzljdUt6a1NERGhWTjYyZAVF5QTZAsY0tVTXJIcDlUbG1jNllXa0ZArSzAyTnlZAXzRaLUZAPYjBUQlF3QVB2OXZAqU25ERkpDYTlWazdrZA3VFSlZABQ3RPaHYyNEVLeWdmYgZDZD";
+const params = new URLSearchParams({
+  fields: "id,caption,media_type,media_url,thumbnail_url,permalink",
+  access_token: accessToken,
+});
+    axios.get(apiUrl + "?" + params).then(response=>{
+        posts.current = response.data.data;
+    });
+},
+[]
+);
+
+console.log(posts.current);
 
     return (
         <div id="social" className="light">
