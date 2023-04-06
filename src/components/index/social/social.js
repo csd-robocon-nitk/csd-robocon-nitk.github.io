e74ad076f1543be50203da15e02a9712cf3fa689
programmer-er <jhariharan2004@gmail.com>
2023-04-06 07:27:44 +0530

    Added Instagram posts in UI on home page

--- src/components/index/social/social.js
+++ src/components/index/social/social.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import './social.css'
 import axios from 'axios';
-import { useEffect,useRef } from 'react';
+import { useEffect,useState } from 'react';
+import Post from './Post'
 
 export default function SocialSection () {
 
-let posts= useRef();
-useEffect(()=>{
-const apiUrl = "https://graph.instagram.com/me/media";
-//console.log(process.env.REACT_APP_IG_KEY);
-const accessToken = window.ig_token;
-const params = new URLSearchParams({
-  fields: "id,caption,media_type,media_url,thumbnail_url,permalink",
-  access_token: accessToken,
-});
-    // console.log("inside useEffect");
-    axios.get(apiUrl + "?" + params).then(response=>{
-        posts.current = response.data.data;
-    }).then(()=>{
-        console.log(posts.current);
-    }).catch(error => {
-        console.log(error);
-    });
-},
-[]
-);
+const [ posts, setPosts ] = useState([]);
+
+useEffect(() => {
+    const apiUrl = "https://graph.instagram.com/me/media";
+    const accessToken = window.ig_token
+    const params = new URLSearchParams({
+        fields: "id,caption,media_type,media_url,thumbnail_url,permalink",
+        access_token: accessToken,
+    })
+        axios.get(apiUrl + "?" + params).then(response=>{
+            setPosts(response.data.data)
+        }).catch(error => {
+            console.log(error);
+        });
+    },
+    []
+)
 
 
 
@@ -35,6 +33,16 @@ const params = new URLSearchParams({
             <div id="videos">
                 <iframe src="https://www.youtube.com/embed/B3DctwOAsaM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <iframe src="https://www.youtube.com/embed/eEnqrpgW8jU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            
+            <br/><br/><br/>
+
+            <h1>SOCIAL</h1>
+            <h2>Keep up with our Updates</h2>
+            <div id="instagram">
+                {
+                    posts.map(post => post.media_type == "IMAGE" ? <Post details={post} /> : null)
+                }
             </div>
         </div>
     )
