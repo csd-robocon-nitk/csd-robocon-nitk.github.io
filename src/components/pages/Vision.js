309790598891ed40a54385c36c7baba1fd7d2955
Shivam Tewari <shivamtewari2002@gmail.cm>
2023-04-08 13:24:02 +0530

    Vision And slight changes in insta card

--- src/components/pages/Vision.js
+++ src/components/pages/Vision.js
@@ -1,16 +1,27 @@
-import React from 'react';
 
-function Vision() {
+import React from "react";
+import './vision.css'
+const Vision = () => { 
   return (
-    <div style={{ backgroundImage: `url(${yourBackgroundImage})` }}>
-      <h1>Our Vision</h1>
-<p> Vision:
-1.To create a culture that embodies a deep drive of learning and encourages creativity and excellence
-2. To consistently build innovative and intelligent machines capable of performing all their tasks in an efficient fashion.
-3. To create machines with a strong underlying influence of ethics and develop practices of  environmentally sustainable construction and creations.
-</p>
-    </div>
+    <div  id = "vision-page" className='dark'>
+     <div id = "nav-placeholder" />
+      <div id="container">
+        <div className="section">
+          <h1>
+           VISION
+          </h1>
+         <p> 1.To create a culture that embodies a deep drive of learning and encourages creativity and excellence 
+        </p>
+        <p>   2. To consistently build innovative and intelligent machines capable of performing all their tasks in an efficient fashion.
+        </p>
+        <p>  3. To create machines with a strong underlying influence of ethics and develop practices of  environmentally sustainable construction and creations.
+         </p>
+          </div>
+        </div>
+      </div>
   );
-}
-
+};
+  
 export default Vision;
+
+
