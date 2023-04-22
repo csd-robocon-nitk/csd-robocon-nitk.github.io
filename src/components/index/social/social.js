7a2bd68a99b55d1bbfc060a195c44b149b42158e
programmer-er <jhariharan2004@gmail.com>
2023-04-22 12:51:37 +0530

    Added arrows for scrolling instagram posts

--- src/components/index/social/social.js
+++ src/components/index/social/social.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './social.css'
 import axios from 'axios';
 import { useEffect,useState } from 'react';
@@ -6,23 +6,39 @@ import Post from './Post'
 
 export default function SocialSection () {
 
-const [ posts, setPosts ] = useState([]);
+    const [ posts, setPosts ] = useState([]);
+    const instagram_container = useRef()
 
-useEffect(() => {
-    const apiUrl = "https://graph.instagram.com/me/media";
-    const accessToken = window.ig_token
-    const params = new URLSearchParams({
-        fields: "id,caption,media_type,media_url,thumbnail_url,permalink",
-        access_token: accessToken,
-    })
-        axios.get(apiUrl + "?" + params).then(response=>{
-            setPosts(response.data.data)
-        }).catch(error => {
-            console.log(error);
-        });
-    },
-    []
-)
+    useEffect(() => {
+        const apiUrl = "https://graph.instagram.com/me/media";
+        const accessToken = window.ig_token
+        const params = new URLSearchParams({
+            fields: "id,caption,media_type,media_url,thumbnail_url,permalink",
+            access_token: accessToken,
+        })
+            axios.get(apiUrl + "?" + params).then(response=>{
+                setPosts(response.data.data)
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        []
+    )
+
+
+    function scrollLeft () {
+        instagram_container.current.scrollBy({ 
+            left: - window.innerWidth / 2,
+            behavior: "smooth"
+        })
+    }
+
+    function scrollRight () {
+        instagram_container.current.scrollBy({ 
+            left: window.innerWidth / 2,
+            behavior: "smooth"
+        })
+    }
 
 
 
@@ -39,10 +55,14 @@ useEffect(() => {
 
             <h1>SOCIAL</h1>
             <h2>Keep up with our Updates</h2>
-            <div id="instagram">
-                {
-                    posts.map(post => post.media_type == "IMAGE" ? <Post details={post} /> : null)
-                }
+            <div id="instagram-container">
+                <div id="instagram" ref={ instagram_container }>
+                    {
+                        posts.map(post => post.media_type == "IMAGE" ? <Post details={post} /> : null)
+                    }
+                </div>
+                <div id="left" onClick={scrollLeft}><span class="material-symbols-outlined">arrow_back_ios</span></div>
+                <div id="right" onClick={scrollRight}><span class="material-symbols-outlined">arrow_forward_ios</span></div>
             </div>
         </div>
     )
