e325989c67c915b0063ab05f7ed3658da2230e99
programmer-er <jhariharan2004@gmail.com>
2023-06-13 12:52:47 +0530

    Added loading animation in homepage

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     HeroContainer,
     HeroBg,
@@ -7,13 +7,27 @@ import {
     HeroP
 } from "./HeroElements";
 import Video from "../../videos/robocon.mp4";
+import logo from "../../images/logo.png"
+
+let randint = (m, n) => parseInt(Math.random() * (n-m+1) + m) 
 
 const HeroSection = () => {
-    const [hover, setHover] = useState(false);
+    let [ visible, setVisible ] = useState(true)
+    let [ progress, setProgress ] = useState(0)
 
-    const onHover = () => {
-        setHover(!hover);
-    };
+    useEffect(() => {
+        if (!visible) return
+        if (progress == 100) {
+            setTimeout(() => setVisible(false), 1000)
+            return
+        }
+
+        setTimeout(() => setProgress(i => {
+            let n = i + randint(4, 8)
+            return n>100 ? 100 : n
+        }), randint(100, 150))
+    })
+
     return (
         <HeroContainer id="home">
             <HeroBg>
@@ -39,6 +53,33 @@ const HeroSection = () => {
                     Karnataka, Surathkal
                 </HeroP>
             </HeroContent>
+            <div style = {{
+                display: "flex",
+                opacity: visible ? 1 : 0,
+                transitionDuration: "800ms",
+                position: "fixed",
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "black",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                color: "white",
+                zIndex: 4
+            }}>
+                <div style={{position: "relative"}}>
+                    <img src = {logo} style = {{ height: "100px" }} />
+                    <div style = {{
+                        height: "100px",
+                        width: `${100-progress}%`,
+                        position: "absolute",
+                        right: 0,
+                        top: 0,
+                        backgroundColor: "#000000aa"
+                    }}/>
+                </div>
+                <p>{progress}%</p>
+            </div>
         </HeroContainer>
     );
 };
