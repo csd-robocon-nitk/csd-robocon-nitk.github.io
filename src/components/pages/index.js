152e22219fa7568e13484b0299bcb18e015e2d0a
programmer-er <jhariharan2004@gmail.com>
2023-06-14 13:58:42 +0530

    Added timeline section on homepage

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -6,11 +6,13 @@ import ContactSection from "../index/contact/contact";
 import ParallaxSection from '../index/parallax/parallax'
 import SponsorsSection from "../index/sponsors/sponsors"
 import SocialSection from '../index/social/social'
+import TimelineSection from '../index/timeline/timeline'
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <TimelineSection />
             <ParallaxSection />
             <SocialSection />
             <AboutSection />
