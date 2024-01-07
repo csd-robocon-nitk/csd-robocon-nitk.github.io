6aeb56dd76755479ac2b261b92862cfb0419695e
Testing wala User <apoorva3569@gmail.com>
2024-01-07 20:02:53 +0530

    Modified team_data and updated home page to 2024 status

--- src/components/index/about/about.js
+++ src/components/index/about/about.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css"
-import image from "../../../images/abu.webp"
+import image from "../../../images/LOGO_ROBOCON2024.png"
 
 export default function AboutSection () {
     return (
@@ -9,7 +9,7 @@ export default function AboutSection () {
                 <h1>ABU ROBOCON</h1>
                 <h2>An Ultimate Robotics Contest for Undergrads</h2>
                 <p>We build Robots that aim at incredible Precision, Reliability and Speed or in other words Industrial performance.</p>
-                <a className="button" href="https://aburobocon2024.vtv.gov.vn/">Learn More</a>
+                <a className="button" href="https://aburobocon2024.vtv.gov.vn/" target="_blank">Learn More</a>
             </div>
             <div id="right">
                 <img src={image} />
