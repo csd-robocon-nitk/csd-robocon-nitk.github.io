2287de125e36ea9ee9469a2710e391b96a967e2a
Hariharan <jhariharan2004@gmail.com>
2024-11-20 23:46:30 +0530

    Updated sponsors page

--- src/components/pages/sponsor.js
+++ src/components/pages/sponsor.js
@@ -1,37 +1,35 @@
 import React from "react"
 import "./sponsor.css"
-import sponsors from "../../images/sponsors.png"
-import { Link } from "react-scroll"
 import SponsorsSection from "../index/sponsors/sponsors"
 
 export default function Sponsor () {
     return (
         <>
             <div className = "nav-placeholder"></div>
-            <SponsorsSection />
             <div id="sponsor-page" className="dark">
                 <div id = "container">
+                    <h1>GET MORE INFO</h1>
+                    <h2>Brochure</h2>
+                    <iframe src="https://drive.google.com/file/d/1R_LZyBsStkSV3K1it_Vn7ZuUzeo6-3sz/preview" width="100%" height="600" allow="autoplay"></iframe>
+                    {/* <iframe src="/assets/CRN Brochure 2024.pdf" width="100%" height="600" allow="autoplay"></iframe> */}
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
+
                     <h1>ENDOWMENT FUND</h1>
                     <h2>Account Details</h2>
                     <br/>
                     <div id = "account">
                         <div><b>NITK/KREC Endowment Fund</b></div>
                         <div><b>Account Number:</b> 37481178720</div>
-                        <div>SBI, NITK Campus Branch, Surathkal, Mangalore - 575 025</div>
+                        <div><b>Branch:</b> SBI, NITK Campus Branch, Surathkal, Mangalore - 575 025</div>
                         <div><b>IFSC:</b> SBIN0002273</div>
                         <br/>
                         <div>ALL PAYMENTS MADE TO CSD ROBOCON NITK ARE SUBJECT TO <b>80G 100% TAX DEDUCTION</b></div>
                     </div>
 
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-
-                    <h1>GET MORE INFO</h1>
-                    <h2>Brochure</h2>
-                    <iframe src="https://drive.google.com/file/d/1INHI8PAVUPyMlsGJEyaKg4sT-WHE-aZ2/preview" width="100%" height="600" allow="autoplay"></iframe>
-                    {/* <iframe src="/assets/CRN Brochure 2024.pdf" width="100%" height="600" allow="autoplay"></iframe> */}
                     <br/>
                     <br/>
                     <br/>
@@ -49,12 +47,12 @@ export default function Sponsor () {
                             </div>
                         </a>
 
-                        <a href = {process.env.PUBLIC_URL + "/assets/CRN 2023 Season Report.pdf"} target = "_blank">
+                        <a href = {process.env.PUBLIC_URL + "/assets/2024/CRN 2024 Season Report.pdf"} target = "_blank">
                             <div>
                                 <span class="material-symbols-outlined">
                                     newspaper
                                 </span>
-                                Season Report 2022-23
+                                Season Report 2023-24
                             </div>
                         </a>
 
@@ -73,9 +71,9 @@ export default function Sponsor () {
                         ** For any queries, contact robocon@nitk.edu.in 
                     </div>
                     
-                    
                 </div>
             </div>
+            <SponsorsSection />
         </>
     )
 }
